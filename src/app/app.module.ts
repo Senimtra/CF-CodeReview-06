0da088fe481caf2d625ebe4d0b9bfa0510936384
Gregor Seegers <gregor.seegers@gmail.com>
2021-03-26 22:42:24 +0100

    add carousel images

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
 import { CartComponent } from './cart/cart.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ReviewPageComponent } from './review-page/review-page.component';
+import { CarouselComponent } from './carousel/carousel.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,6 +23,7 @@ import { ReviewPageComponent } from './review-page/review-page.component';
     ProductDetailsComponent,
     CartComponent,
     ReviewPageComponent,
+    CarouselComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
