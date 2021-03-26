94b0d5bd4c9016b53df920da9c4d813edf1972ee
Gregor Seegers <gregor.seegers@gmail.com>
2021-03-26 19:06:09 +0100

    add total sum and discount to cart

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -11,6 +11,9 @@ export class CartComponent implements OnInit {
   items;
   checkoutForm;
   sumPrice;
+  discountValue;
+  discountRate;
+  endTotal;
 
   constructor(
     private cartService: CartService,
@@ -25,11 +28,25 @@ export class CartComponent implements OnInit {
   ngOnInit() {
     this.items = this.cartService.getItems();
     this.sumPrice = 0;
+    this.discountValue = 0;
+    this.discountRate = 0;
+    this.endTotal = 0;
     for (let i in this.items) {
       // console.log(this.items[i].price);
       this.sumPrice = this.sumPrice + this.items[i].price;
       console.log(this.sumPrice);
     }
+    if (this.sumPrice > 500) {
+      console.log('20% discount');
+      this.discountValue = this.sumPrice * 0.2;
+      this.discountRate = 20;
+      this.endTotal = this.sumPrice - this.discountValue;
+    } else if (this.sumPrice > 200) {
+      console.log('10% discount');
+      this.discountValue = this.sumPrice * 0.1;
+      this.discountRate = 10;
+      this.endTotal = this.sumPrice - this.discountValue;
+    } else console.log('no discount');
   }
 
   onSubmit(customerData) {
